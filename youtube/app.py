cffb72064316ed3ff179cb7bf7715eb09b355958
Fernanda Dagostin <33377159+fernandadagostin@users.noreply.github.com>
2020-03-03 22:21:29 -0500

    datatable with Tab

--- youtube/app.py
+++ youtube/app.py
@@ -1,40 +1,44 @@
-
+from difflib import SequenceMatcher
+from  Tkinter import Tk
+import Tkconstants, tkFileDialog
 import os
+import glob
 
 
+myDir= str('myfolder')
+playlist = 'playlist_url'
 
-playlist = 'https://www.youtube.com/playlist?list=PL6Lt9p1lIRZ311J9ZHuzkR5A3xesae2pk'
-
-#print('python youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 '+playlist)
-#os.system('youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 '+playlist)
-# os.system('youtube-dl -ic "%(artist)s - %(title)s" --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 '+playlist)
-os.system('youtube-dl -i -o "%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 '+ playlist)
+os.system('youtube-dl --download-archive downloaded.txt --no-overwrites -i -o "./'+myDir+'/%(title)s.%(ext)s" --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 '+ playlist)
+   
 '''
--i - ignore errors
+    Parameters on youtube-dl
 
--c - continue
+    -i - ignore errors
 
--t - use video title as file name
+    -c - continue
 
---extract-audio - extract audio track
+    -t - use video title as file name
 
---audio-format mp3 - convert to mp3
+    --extract-audio - extract audio track
 
---audio-quality 0 - the best audio quality
+    --audio-format mp3 - convert to mp3
 
---yes-playlist - affirm that url points to a playlist
+    --audio-quality 0 - the best audio quality
 
-YT_URL - video url from youtube
+    --yes-playlist - affirm that url points to a playlist
 
-# Download single entry
-youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 YT_URL
+    YT_URL - video url from youtube
 
-# Download playlist
-youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
+    # Download single entry
+    youtube-dl -i --extract-audio --audio-format mp3 --audio-quality 0 YT_URL
 
-# Download playlist, --download-archive downloaded.txt add successfully downloaded files into downloaded.txt
-youtube-dl --download-archive downloaded.txt --no-overwrites -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
+    # Download playlist
+    youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
 
-# Retry until success, no -i option
-while ! youtube-dl --download-archive downloaded.txt --no-overwrites -ct --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 <YT_PlayList_URL>; do echo DISCONNECTED; sleep 5; done
+    # Download playlist, --download-archive downloaded.txt add successfully downloaded files into downloaded.txt
+    youtube-dl --download-archive downloaded.txt --no-overwrites -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 https://www.youtube.com/playlist?list=UUCvVpbYRgYjMN7mG7qQN0Pg
+
+    # Retry until success, no -i option
+    while ! youtube-dl --download-archive downloaded.txt --no-overwrites -ct --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 --socket-timeout 5 <YT_PlayList_URL>; do echo DISCONNECTED; sleep 5; done
 '''
+
